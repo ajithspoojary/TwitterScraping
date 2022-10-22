20dbe8d4edb924d7d4fd66a1bbe366a3ae5b6384
connectasp <connect.asp90@gmail.com>
2022-10-22 16:19:05 +0530

    2nd Update

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import streamlit as st
+import datetime
+import pandas as pd
 st.set_page_config(page_title="Twitter Scrapper", page_icon=":tada:", layout="wide")
 
 # header sect
 st.header("Twitter Scrapping Project")
 st.title("First Twitter Project")
 
+tweet = st.text_input("Enter the hash tweet", "Type Here ...")
+
+
+if (st.button('Submit')):
+    result = tweet.title()
+    st.success(result)
+
+srt = st.date_input(
+    "Enter the Start date of tweet",
+    datetime.date(2021, 1, 1))
+ed = st.date_input(
+    "Enter the Start date of tweet",
+    datetime.date(2022, 10, 2))
+
+st.write("Your data is range is from ", srt, ed)

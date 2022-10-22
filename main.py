3785498fa3e07dbb66a3880e159a60f69345c5ac
connectasp <connect.asp90@gmail.com>
2022-10-22 23:01:42 +0530

    UI Update

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ st.set_page_config(page_title="Twitter Scrapper", page_icon=":tada:", layout="wi
 
 # header sect
 st.header("Twitter Scrapping Project")
-st.title("First Twitter Project")
+#st.title("First Twitter Project")
 
-tweet = st.text_input("Enter the hash tweet", "Type Here ...")
+tweet = st.text_input("Enter the Twitter hashtag", "Type Here ...")
+nt = st.text_input("Enter the no of Tweets", "Type Here ...")
 
 
 if (st.button('Submit')):
@@ -15,10 +16,10 @@ if (st.button('Submit')):
     st.success(result)
 
 srt = st.date_input(
-    "Enter the Start date of tweet",
+    "Enter the Start date of HashTag",
     datetime.date(2021, 1, 1))
 ed = st.date_input(
-    "Enter the Start date of tweet",
+    "Enter the Start date of HashTag",
     datetime.date(2022, 10, 2))
 
 st.write("Your data is range is from ", srt, ed)

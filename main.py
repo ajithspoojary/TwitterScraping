f1e16545ca83cc414e3f903ca9743a5750de63cb
connectasp <connect.asp90@gmail.com>
2022-10-23 11:25:13 +0530

    UI Update

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import datetime
 import pandas as pd
-st.set_page_config(page_title="Twitter Scrapper", page_icon=":tada:", layout="wide")
+st.set_page_config(page_title="Twitter Scrapper", page_icon=":tada:", layout="centered")
 
 # header sect
 st.header("Twitter Scrapping Project")
 #st.title("First Twitter Project")
 
-tweet = st.text_input("Enter the Twitter hashtag", "Type Here ...")
-nt = st.text_input("Enter the no of Tweets", "Type Here ...")
+tweet = st.text_input("Enter the Twitter hashtag",)
+if tweet == "":
+    st.stop()
+nt = st.number_input('Insert a number of tweets to search', min_value=1, max_value=100000, value = 1, step=1)
 
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+start_date = st.date_input('Start date',today)
+end_date = st.date_input('End date',tomorrow)
 
 if (st.button('Submit')):
-    result = tweet.title()
-    st.success(result)
-
-srt = st.date_input(
-    "Enter the Start date of HashTag",
-    datetime.date(2021, 1, 1))
-ed = st.date_input(
-    "Enter the Start date of HashTag",
-    datetime.date(2022, 10, 2))
-
-st.write("Your data is range is from ", srt, ed)
+    if start_date < end_date:
+        st.success('Date Validated')
+    else:
+        st.error('Error: End date must fall after start date.')

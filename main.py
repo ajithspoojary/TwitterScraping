37a0b536da544e4db0694708b56b6146669eca01
connectasp <connect.asp90@gmail.com>
2022-10-25 22:36:40 +0530

    UI Update

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 from pymongo import MongoClient
 from streamlit_lottie import st_lottie
 import lottie as lo
-
-
+import time
+import json
 
 st.set_page_config(page_title="Twitter Scrapper", page_icon=":tada:", layout="wide")
 
@@ -18,14 +18,12 @@ with title_container:
     with col2:
         st.markdown('<h1 style="color: purple;">Twitter HashTag Scrapper</h1>', unsafe_allow_html=True)
 
-
-
 with st.sidebar:
-    st_lottie(lo.lottie_anim.lottie_coding1)
+    st_lottie(lo.lottie_anim.lottie_coding1, height=300)
 
 
-tweet = st.sidebar.text_input("Enter the Twitter hashtag")
-if tweet == "":
+tweet_keyword = st.sidebar.text_input("Enter the Twitter hashtag")
+if tweet_keyword == "":
     st.stop()
 
 nt = st.sidebar.number_input('Insert a number of tweets to search', min_value=1, max_value=100000, value = 1, step=1)
@@ -36,54 +34,83 @@ start_date = st.sidebar.date_input('Start date',today)
 end_date = st.sidebar.date_input('End date',tomorrow)
 
 if (st.sidebar.button('Submit')):
-    if tweet == "":
-        st.error("Enter the Hastag to search")
+    #if tweet == "":
+        #st.error("Enter the Hastag to search")
+
     if start_date < end_date:
-        st.sidebar.success('Date Validated')
+        #st.sidebar.success('Date Validated')
+
+        with st.spinner('Loading..........'):
+            time.sleep(5)
+        st.success('Data Successfully Extracted!')
+
+
+        #my_bar = st.sidebar.progress(0)
+        #for percent_complete in range(100):
+            #time.sleep(0.001)
+            #my_bar.progress(percent_complete + 1)
+
         # Query by text search
         # Setting variables to be used below
         # Creating list to append tweet data to
-        tweets_list2 = []
+        tweets_list = []
 
         # Using TwitterSearchScraper to scrape data and append tweets to list
-        for i, tweet in enumerate(
-                sntwitter.TwitterSearchScraper(f'#{tweet} since:{start_date} until:{end_date}').get_items()):
+        for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'#{tweet_keyword} since:{start_date} until:{end_date}').get_items()):
             if i > nt:
                 break
-            tweets_list2.append([ tweet.id,tweet.user.username,tweet.user.followersCount, tweet.content,  tweet.likeCount, tweet.retweetCount, tweet.lang, tweet.replyCount,tweet.date, tweet.url,  tweet.source])
+            tweets_list.append([tweet.id,tweet.user.username,tweet.user.followersCount, tweet.content,  tweet.likeCount, tweet.retweetCount, tweet.lang, tweet.replyCount,tweet.date, tweet.url,  tweet.source, tweet.sourceLabel,tweet_keyword])
 
         # Creating a dataframe from the tweets list above
-        tweets_df2 = pd.DataFrame(tweets_list2,columns=['Tweet Id', 'Username', 'Followers Count',  'Content', 'Likes', 'Retweet Count', 'Language', 'Reply Count', 'Date-time',  'URL', 'Source'])
+        tweets_df = pd.DataFrame(tweets_list,columns=['Tweet Id', 'Username', 'Followers Count',  'Content', 'Likes', 'Retweet Count', 'Language', 'Reply Count', 'Date-time',  'URL', 'Source', 'Source Label', 'Keyword'])
         # Display first 5 entries from dataframe
-        tweets_df2.head()
-        #tweets_df2['Hashtag_Keyword'] = tweet
+        #tweets_df.head()
 
+        st.write(tweets_df)
+        def convert_df(tweets_df):
+            return tweets_df.to_csv(index=False).encode('utf-8')
+        csv = convert_df(tweets_df)
+
+        # def convert_jsondf(tweets_df2):
+        # return tweets_df2.to_json
+        # json = convert_jsondf(tweets_df2)
+
+        #json_string = json.dumps(tweets_list2, default=str)
         # Export dataframe into a CSV
-        tweets_df2.to_csv('text-query-tweets.csv', sep=',', index=False)
-        st.write(tweets_df2)
-        def convert_df(tweets_df2):
-            return tweets_df2.to_csv(index=False).encode('utf-8')
-        csv = convert_df(tweets_df2)
-        col1, col2, col3 = st.columns([1, 1, 1])
+        tweets_df.to_csv('text-query-tweets.csv', sep=',', index=False)
 
+        #def convert_jsondf(tweets_df):
+            #return tweets_df.to_json
+        #json_string = convert_jsondf(tweets_df)
+        json_string = tweets_df.to_json(orient='index')
+        #json_string = json.dumps(tweets_list,default=str)
+        #st.json(json_string, expanded=True)
+
+        col1, col2, col3 = st.columns([1, 1, 1])
         with col1:
             #st.button('Upload to Database')
-            if (st.button('Upload To Database')):
+            if st.button('Upload To Database'):
                 # Making a Connection with MongoClient
-                client = MongoClient("mongodb://localhost:27017/")
+                client = MongoClient("mongodb://localhost:27017")
+                st.success("Connection Successfull")
                 # database
                 db = client["Tweet_Scrap"]
+                #db = client.Twitter_Scrap
+                st.success("Database Created")
                 # collection
                 tweet_db = db["Hash_detail"]
-                tweet_db.insert(tweets_df2.to_dict())
+                #tweet_db = db.Hash_detail
+                st.success("Collection Created")
+                tweet_db.insert_many(json_string)
                 st.success("Details Uploaded Successfully")
 
         with col2:
-            st.button("Download JSON File")
+            #st.download_button(label="Download JSON File ",file_name="data.json",mime="application/json",data=json_string)
+            st.download_button("Download JSON File", json_string, "tweetreport.json", "application/json", key='download-json')
+
         with col3:
             st.download_button("Download CSV File", csv, "tweetreport.csv", "text/csv", key='download-csv')
 
-        #st.write(tweets_list2)
-        #st.write("Success in Reading Tweets!!!!")
+
     else:
         st.error('Error: End date must fall after start date.')
